1ebd51dd50c9004fb1c05a10c811e5f7572f8ffd
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2015-02-27 07:46:17 +0100

    First commit, but the project is not in a beginning state.

---
+++ FitnessTracking/WebContent/WEB-INF/js/App.js
@@ -0,0 +1,90 @@
+
+var adjustCircleProgress = function (jQueryObj, value) {
+	var size = 150;
+	if (window.innerWidth < 768) {
+		size = 140;
+	}
+	
+	jQueryObj.circleProgress({
+	    value: value,
+	    size: size,
+	    lineCap: 'round',
+	    thickness: 30,
+	    animation: {
+            duration: 3500,
+            easing: 'circleProgressEasing'
+        }
+	}).on('circle-animation-progress', function(event, progress) {
+		$(this).find('strong').html(parseInt(value * 100 * progress) + '<i>%</i>');
+	});
+};
+
+var adjustLineChart = function (chartID, columns) {
+	var chart = c3.generate({
+	    bindto: '#' + chartID,
+	    data: {
+	      columns: columns
+	    }
+	});
+};
+
+$(document).ready(function() {
+	var table = $('#dataTable');
+	var circleProgresses = $('#circleProgresses');
+	var data = trackdata;
+	
+	var totalRatioColumn = ['Total Ratio'];
+	var totalRatioColumns = [totalRatioColumn];
+	var calorieDuration = ['Calories / Duration'];
+	var calorieDurations = [calorieDuration];
+	
+	data.forEach(function(workout) {
+		var date = new Date(workout.date);
+		workout.date = date;
+		
+		//
+		var fatBurnProgress = workout.fatBurnDuration / workout.duration;
+		var fitnessProgress = workout.fitnessDuration / workout.duration;
+		
+		workout.fatBurnProgress = fatBurnProgress;
+		workout.fitnessProgress = fitnessProgress;
+		
+		//
+		var totalRatio = workout.calories * workout.averageBPM * workout.maxBPM * workout.fatBurnDuration / workout.duration;
+		workout.totalRatio = totalRatio;
+		totalRatioColumn.push(totalRatio);
+		
+		var calorDur = workout.calories / workout.duration;
+		calorieDuration.push(calorDur.toPrecision(2));
+	});
+	
+	// populate circle progress
+	data.forEach(function(workout, index) {
+		var c = document.createElement('div');
+		var circ = $(c).addClass('circle').addClass('circle-' + index);
+		var s = document.createElement('strong');
+		var i = document.createElement('div');
+		circ.append(i);
+		circ.append(s);
+		circleProgresses.append(circ);
+		adjustCircleProgress(circ, workout.fatBurnProgress);
+	});
+	
+	// populate table
+	var rev = data.reverse();
+	rev.forEach(function(workout, index) {
+		var row = '<tr><td>' + workout.date.toDateString()
+			+ '</td><td>' + workout.date.toTimeString()
+			+ '</td><td>' + workout.duration.toHHMMSS()
+			+ '</td><td>' + workout.calories.tokCal()
+			+ '</td><td>' + workout.averageBPM.toBPM()
+			+ '</td><td>' + workout.maxBPM.toBPM()
+			+ '</td><td>' + workout.fatBurnDuration.toHHMMSS()
+			+ '</td><td>' + workout.fitnessDuration.toHHMMSS()
+			+ '</td></tr>';
+		table.append(row);
+	});
+	
+	adjustLineChart('totalRatio', totalRatioColumns);
+	adjustLineChart('caloriesDuration', calorieDurations);
+});
